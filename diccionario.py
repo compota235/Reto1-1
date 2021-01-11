d84c55fdc2f0882ada660ef7a79cb668929409b6
marvinjoel <marvin.roses@gmail.com>
2021-01-11 11:57:09 -0500

    Dicionario python

---
+++ diccionario.py
@@ -0,0 +1,13 @@
+# DICCIONARIO
+
+# print(dicionario['azul']) para saber que resultado trae
+# dicionario["Perro"] = 'Dog' para qgregar o modificar
+# del(dicionario['Azul']), elimina
+# print(dicionario.get(7, " No existe lo que esta buscando")), para controlar los errores
+# print(dicionario.keys()) muestra solo las claves
+# print(dicionario.values()) muestra todos los valorres
+
+dicionario = {'Azul':'Blue','Rojo':'Red','Amerilla':'Lleyow'}
+dicionario[7]='Hola Mundo'
+
+print(dicionario.items())
